f60a8ec141e134332087cf5be1199b9d68caf8af
Josh McArthur <joshua.mcarthur@gmail.com>
2013-09-17 12:52:58 +1200

    Spec FirstClickFree::Concerns::Controller

--- spec/concerns/controller_spec.rb
+++ spec/concerns/controller_spec.rb
@@ -1,20 +1,66 @@
 require "spec_helper"
 
 describe FirstClickFree::Concerns::Controller, type: :controller do
-  controller do
-    include described_class
+  context "standard controller" do
+    controller do
+      include FirstClickFree::Concerns::Controller
+      allow_first_click_free
 
-    def index
-      head :ok
+      def index
+        head :ok
+      end
+    end
+
+    context "first visit" do
+      before { get :index }
+      it { session[:first_click].should_not be_nil }
+      it { response.should be_success }
+    end
+
+    context "subsequent visit" do
+      before { session[:first_click] = Time.zone.now }
+      it { expect { get :index }.to raise_error FirstClickFree::Exceptions::SubsequentAccessException }
+    end
+
+    context "googlebot visit" do
+      before { controller.stub(:googlebot? => true) }
+      it { session[:first_click].should be_nil }
+      it { response.should be_success }
     end
   end
 
-  context "first visit" do
+  context "controller skipping an action" do
+    controller do
+      include FirstClickFree::Concerns::Controller
+      allow_first_click_free except: :index
+
+      def index
+        head :ok
+      end
+    end
+
     before { get :index }
-    it { session[:first_click].should_not be_nil }
+    it { session[:first_click].should be_nil }
     it { response.should be_success }
   end
 
-  context "subsequent visit"
-  context "googlebot visit"
+  context "entire controller skipped" do
+    controller do
+      include FirstClickFree::Concerns::Controller
+
+      # Normally this would be done in ApplicationController or similar
+      allow_first_click_free
+
+      # And this would be done in a child controller
+      skip_first_click_free
+
+      def index
+        head :ok
+      end
+    end
+
+    before { get :index }
+    it { session[:first_click].should be_nil }
+    it { response.should be_success }
+  end
 end
