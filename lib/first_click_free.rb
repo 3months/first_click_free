6b168c698c4aecdc1dfd6a132b20b61e544b4906
Josh McArthur <joshua.mcarthur@gmail.com>
2013-09-17 12:52:48 +1200

    Require First click free exceptions

--- lib/first_click_free.rb
+++ lib/first_click_free.rb
@@ -1,4 +1,5 @@
 module FirstClickFree
+  require 'first_click_free/exceptions/subsequent_access_exception'
   require 'first_click_free/helpers/google'
   require 'first_click_free/concerns/controller'
 end
