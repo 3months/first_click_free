87d1332784ba3348eee37f3f02b3769a22db2ece
Josh McArthur <joshua.mcarthur@gmail.com>
2013-09-24 10:44:20 +1200

    Add class helpers for FirstClickFree module for accessing permitted domains

--- lib/first_click_free.rb
+++ lib/first_click_free.rb
@@ -3,6 +3,16 @@ module FirstClickFree
   require 'first_click_free/helpers/google'
   require 'first_click_free/concerns/controller'
 
+  class << self
+    def root
+      File.expand_path(File.join(File.dirname(__FILE__), '..'))
+    end
+
+    def permitted_domains
+      @permitted_domains ||= YAML.load_file(File.join(FirstClickFree.root, 'config', 'domains.yml'))
+    end
+  end
+
   class Railtie < Rails::Railtie
     initializer "first_click_free.action_controller" do
       ActiveSupport.on_load(:action_controller) do
